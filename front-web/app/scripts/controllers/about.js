79dd1e5057b6d535b604ed36c1492d9c531d6085
xiangyu <jixiangyu@wandoujia.com>
2015-11-27 08:08:30 +0800

    fix jade

---
+++ front-web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontWebApp
+ */
+angular.module('frontWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ front-web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontWebApp
+ */
+angular.module('frontWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
